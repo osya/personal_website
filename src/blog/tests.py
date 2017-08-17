3e8e376ae068390dc29afb972fd9bd4da4ac23a4
Valeriy Osipov <info@vosipov.com>
2017-08-17 21:00:42 +0300

    Add unit test

--- src/blog/tests.py
+++ src/blog/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from blog.models import Post
 
-# Create your tests here.
+
+class PostTests(TestCase):
+    def test_str(self):
+        p = Post(title='MyTitle', description='MyDescription', content='MyContent', is_commentable=False)
+        self.assertEqual(str(p), 'MyTitle')
