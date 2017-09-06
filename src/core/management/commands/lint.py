3d3043226297479fbe2ef0c3213db0798b63b0cd
Valeriy Osipov <info@vosipov.com>
2017-09-06 10:34:33 +0300

    Fix path for flake8

--- src/core/management/commands/lint.py
+++ src/core/management/commands/lint.py
@@ -29,7 +29,7 @@ class Command(BaseCommand):
             if rv is not 0:
                 exit(rv)
 
-        scripts_path = os.path.join(os.path.dirname(sys.executable), 'Scripts')
+        scripts_path = os.path.join(os.path.dirname(sys.executable), 'Scripts' if os.name == 'nt' else 'bin')
         if options['fix_imports']:
             execute_tool('Fixing import order', os.path.join(scripts_path, 'isort'), '-rc')
         execute_tool('Checking code style', os.path.join(scripts_path, 'flake8'))
