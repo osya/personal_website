b8193c533a827f0d8fe6d29599fd1027936fbaaf
Valeriy Osipov <info@vosipov.com>
2018-02-20 00:28:37 +0300

    Fix PyLint C0122 warnings

--- src/post/tests.py
+++ src/post/tests.py
@@ -14,7 +14,8 @@ from post.models import Post
 from post.views import PostList
 
 
-def random_string_generator(size=10, chars=string.ascii_lowercase + string.digits):
+def random_string_generator(size=10,
+                            chars=string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 
@@ -23,7 +24,8 @@ class UserFactory(factory.DjangoModelFactory):
         model = get_user_model()
 
     username = factory.Sequence(lambda n: 'Agent %03d' % n)
-    email = factory.LazyAttributeSequence(lambda o, n: f'{o.username}{n}@example.com')
+    email = factory.LazyAttributeSequence(
+        lambda o, n: f'{o.username}{n}@example.com')
     password = factory.PostGenerationMethodCall('set_password')
 
 
@@ -53,14 +55,20 @@ class BlogViewTests(TestCase):
         request = self.factory.get('/')
         request.user = UserFactory(password=random_string_generator())
         response = PostList.as_view()(request)
-        self.assertEquals(list(response.context_data['latest']), [], )
+        self.assertEquals(
+            list(response.context_data['latest']),
+            [],
+        )
 
     def test_posts_in_context(self):
         request = self.factory.get('/')
         post = PostFactory()
         request.user = post.user
         response = PostList.as_view()(request)
-        self.assertEquals(list(response.context_data['latest']), [post], )
+        self.assertEquals(
+            list(response.context_data['latest']),
+            [post],
+        )
 
 
 class IntegrationTests(LiveServerTestCase):
@@ -69,9 +77,10 @@ class IntegrationTests(LiveServerTestCase):
     @classmethod
     def setUpClass(cls):
         cls.selenium = WebDriver(
-            executable_path=os.path.join(os.path.dirname(settings.BASE_DIR), 'node_modules', 'phantomjs-prebuilt',
-                                         'lib', 'phantom', 'bin', 'phantomjs')
-        ) if 'nt' == os.name else WebDriver()
+            executable_path=os.path.join(
+                os.path.dirname(settings.BASE_DIR), 'node_modules',
+                'phantomjs-prebuilt', 'lib', 'phantom', 'bin',
+                'phantomjs')) if os.name == 'nt' else WebDriver()
         cls.password = random_string_generator()
         super(IntegrationTests, cls).setUpClass()
 
@@ -99,23 +108,32 @@ class IntegrationTests(LiveServerTestCase):
         self.assertIn(response.status_code, (301, 302))
 
     def test_post_create(self):
-        self.assertTrue(self.client.login(username=self.user.username, password=self.password))
+        self.assertTrue(
+            self.client.login(
+                username=self.user.username, password=self.password))
         cookie = self.client.cookies[settings.SESSION_COOKIE_NAME]
         # Replace `localhost` to 127.0.0.1 due to the WinError 10054 according to the
         # https://stackoverflow.com/a/14491845/1360307
-        self.selenium.get(f'{self.live_server_url}{reverse("post:create")}'.replace('localhost', '127.0.0.1'))
+        self.selenium.get(
+            f'{self.live_server_url}{reverse("post:create")}'.replace(
+                'localhost', '127.0.0.1'))
         if cookie:
             self.selenium.add_cookie({
                 'name': settings.SESSION_COOKIE_NAME,
                 'value': cookie.value,
                 'secure': False,
                 'path': '/',
-                'domain': '127.0.0.1'  # it is needed for PhantomJS due to the issue
+                'domain':
+                '127.0.0.1'  # it is needed for PhantomJS due to the issue
                 # "selenium.common.exceptions.WebDriverException: Message: 'phantomjs' executable needs to be in PATH"
             })
         self.selenium.refresh()  # need to update page for logged in user
         self.selenium.find_element_by_id('id_title').send_keys('post title')
         self.selenium.find_element_by_id('id_body').send_keys('post body')
-        self.selenium.find_element_by_xpath('//*[@id="submit-id-save"]').click()
+        self.selenium.find_element_by_xpath(
+            '//*[@id="submit-id-save"]').click()
         self.assertEqual(1, Post.objects.count())
         self.assertEqual('post title', Post.objects.first().title)
+
+
+# TODO: Selenium support for PhantomJS has been deprecated, please use headless versions of Chrome or Firefox instead
