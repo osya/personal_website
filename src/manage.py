c9d4cda78e39142eeef9aa0d2139ccd073954e38
Valeriy Osipov <info@vosipov.com>
2018-02-21 01:15:10 +0300

    Refactor supressing PyLint W0611

--- src/manage.py
+++ src/manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_website.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'personal_website.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
@@ -11,10 +12,11 @@ if __name__ == '__main__':
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
         try:
-            import django  # noqa  # pylint: disable=W0611
+            import django  # noqa pylint: disable=W0611
         except ImportError:
-            raise ImportError('Couldn\'t import Django. Are you sure it\'s installed and '
-                              'available on your PYTHONPATH environment variable? Did you '
-                              'forget to activate a virtual environment?')
+            raise ImportError(
+                'Couldn\'t import Django. Are you sure it\'s installed and '
+                'available on your PYTHONPATH environment variable? Did you '
+                'forget to activate a virtual environment?')
         raise
     execute_from_command_line(sys.argv)
