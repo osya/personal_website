cec2dbb5dec7a09fce1299672c5da085db51fb89
Valeriy Osipov <info@vosipov.com>
2018-02-26 23:15:11 +0300

    Refactoring: use AutoSlugField

--- src/post/models.py
+++ src/post/models.py
@@ -1,8 +1,9 @@
+# noinspection PyPackageRequirements
+from autoslug import AutoSlugField
 from django.conf import settings
 from django.db import models
 from django.db.models import Q
 from django.urls import reverse
-from django.utils.text import slugify
 from django_markdown.models import MarkdownField
 from taggit_selectize.managers import TaggableManager
 
@@ -23,15 +24,18 @@ class PostQuerySet(models.QuerySet):
 
 
 class Post(models.Model):
+    # pragma pylint: disable=R0903
     class Meta:
         ordering = ('-created', )
         verbose_name = 'Blog Post'
         verbose_name_plural = 'Blog Posts'
 
+    # pragma pylint: enable=R0903
+
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     title = models.CharField(max_length=100, unique_for_date='created')
     body = MarkdownField()
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True)
     is_commentable = models.BooleanField(default=True)
     tags = TaggableManager(blank=True)
     created = models.DateTimeField(auto_now_add=True, db_index=True)
@@ -45,8 +49,3 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post:detail', kwargs={'slug': self.slug})
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title, allow_unicode=True)
-        super(Post, self).save(*args, **kwargs)
